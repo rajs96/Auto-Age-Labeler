a8c8db9d514127cefca6b0f3cc3dcc8860fca474
Raj Singh <rajesh.singh@vanderbilt.edu>
2019-10-21 22:15:21 -0400

    updated helper methods

--- ML-modeling/feature_selection.py
+++ ML-modeling/feature_selection.py
@@ -9,7 +9,7 @@
 
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectFromModel,SelectKBest,chi2
 from sklearn.ensemble import ExtraTreesClassifier
 
 
@@ -23,7 +23,8 @@ def lasso_feature_selection(X,y,penalty=0.01,model='svm',threshold=0.3):
         model -- chosen linear model for applying L1 regularization
         threshold -- if parameter has a weight lower than 0.3, delete it
     Returns:
-        X_transformed -- new dataset with features selected.
+        X_transformed (tuple) -- new dataset with features selected, along
+        with object that transforms the data
     """
     if model == 'svm':
         lasso_model = LinearSVC(penalty='l1',C=penalty,dual=False)
@@ -36,7 +37,7 @@ def lasso_feature_selection(X,y,penalty=0.01,model='svm',threshold=0.3):
 
     # transform data using L1 regularization
     X_transformed = clf.transform(X)
-    return X_transformed
+    return (X_transformed,clf)
 
 def tree_based_feature_selection(X,y,n_estimators=50):
     """Function that returns dataset with tree-based feature selection applied
@@ -47,10 +48,28 @@ def tree_based_feature_selection(X,y,n_estimators=50):
         n_estimators -- number of trees used in the feature selector
 
     Returns:
-        X_transformed -- new dataset with features selected.
+        X_transformed (tuple) -- new dataset with features selected, along
+        with object that transforms the data
     """
     tree_model = ExtraTreesClassifier(n_estimators=n_estimators)
     tree_model.fit(X,y)
     model = SelectFromModel(tree_model,prefit=True)
     X_transformed = model.transform(X)
-    return X_transformed
+    return (X_transformed,model)
+
+def chi_squared_feature_selection(X,y,k=80):
+    """Function that returns dataset with chi-squared based feature selection
+
+    Args:
+        X -- training dataset for inputs
+        y -- training dataset for outputs
+        k -- number of features to select
+
+    Returns:
+        X_transformed (tuple) -- new dataset with features selected, along
+        with object that transforms the data
+    """
+    transformer = SelectKbest(chi2,k=k)
+    X_transformed = transformer.fit_transform(X,y)
+    X_transformed = SelectKBest(chi2,k=k).fit_transform(X,y)
+    return (X_transformed,transformer)
--- ML-modeling/models.py
+++ ML-modeling/models.py
@@ -1,14 +1,31 @@
 # necessary imports
 from keras.layers import Dense,Dropout
 from keras.models import Model,Sequential
+from keras import optimizers
 
 
-def create_model(optimizer='adam',dropout=0.2,output_classes=8):
+def baseline_model(output_classes=8):
+    """Returns a very simple keras classification model
+    """
+    model = Sequential()
+    model.add(Dense(10,activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(output_classes,activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',optimizer='adam',
+        metrics=['accuracy'])
+    return model
+
+    #
+def create_model(dropout=0.2,learning_rate=0.01,
+        output_classes=8):
     """Returns a Keras classification NN that can be wrapped by sklearn
 
     Args:
         optimizer -- the optimizer you want to use for the NN
         dropout -- the dropout (1-keep_prob) for the NN
+        learning_rate -- tunable learning rate parameter for SGD
+        momentum -- tunable momentum parameter for optimizer
         ouput_classes - number of output classes in the model
 
     Returns:
@@ -24,11 +41,13 @@ def create_model(optimizer='adam',dropout=0.2,output_classes=8):
     model.add(Dropout(dropout))
     model.add(Dense(256,activation='relu'))
     model.add(Dropout(dropout))
-    model.add(Dense(256,activation='relu'))
+    model.add(Dense(128,activation='relu'))
     model.add(Dense(output_classes,activation='softmax'))
 
+    model_opt = optimizers.Adam(lr=learning_rate)
+
     # compile the model with optimizer, metrics
-    model.compile(loss='categorical_crossentropy',optimizer=optimizer,
+    model.compile(loss='categorical_crossentropy',optimizer=model_opt,
         metrics=['accuracy'])
 
     return model
