ffd778b34d7cf59c928472cf61656581e76bf73d
Raj Singh <rajesh.singh@vanderbilt.edu>
2019-12-27 15:22:34 -0500

    added back build folder

---
+++ webapp/client/build/precache-manifest.fffe7a93901b04ac5f4908a40e2dcd6f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd30e68d73158297566dc5ea5370aab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "625aff3d5aa022b87c33",
+    "url": "/static/js/2.99e00a9c.chunk.js"
+  },
+  {
+    "revision": "612cbee67c3e0fb33565",
+    "url": "/static/js/main.1ea3acc9.chunk.js"
+  },
+  {
+    "revision": "cb0a348e0fc00505f5cc",
+    "url": "/static/js/runtime-main.4345266c.js"
+  }
+]);
